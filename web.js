9c85ef0b384f76b341402178d3f6267684c7c8f9
Bob Beban <bobbeban@kink.com>
2017-08-09 16:36:12 -0700

    KIN-5638

--- web.js
+++ web.js
@@ -6,28 +6,9 @@ module.exports = {
 	],
 	env: {
 		browser: true,
-		es6: false,
 		jquery: true,
 		mocha: true,
 		mongo: false,
 		node: false
-	},
-	parserOptions: {
-		ecmaVersion: 5
-	},
-	rules: {
-		// http://eslint.org/docs/rules/no-var
-		'no-var': 'off',
-		// http://eslint.org/docs/rules/object-shorthand
-		'object-shorthand': 'off',
-		// http://eslint.org/docs/rules/prefer-const
-		'prefer-const': 'off',
-		// http://eslint.org/docs/rules/one-var
-		'one-var': [
-			'error',
-			'always'
-		],
-		// http://eslint.org/docs/rules/prefer-template
-		'prefer-template': 'off'
 	}
 };
