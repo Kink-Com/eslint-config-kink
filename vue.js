eefbdca5f45ddbdf3174ecb8d9fdf813aef42b01
atomboyd <atomboyd@gmail.com>
2021-01-27 14:32:01 -0800

    KIN-12824: stash attack

--- vue.js
+++ vue.js
@@ -2,6 +2,7 @@ module.exports = {
 	extends: [
 		'kink',
 		'plugin:vue/recommended',
+		'plugin:jsdoc',
 	],
 	env: {
 		browser: true,
@@ -10,18 +11,31 @@ module.exports = {
 		mongo: false,
 		node: false,
 	},
-	rules: {
-		// Conflict with Node 6 and Vue; remove when we upgrade to Node 8.
-		'node/no-unsupported-features': 0,
-		'node/no-unsupported-features/es-syntax': 'off',
-		// `html-indent` does not currently support spaces & since we are all
-		// tabs everywhere, let's keep it that way!
-		'vue/html-indent': 0,
-	},
 	plugins: [
 		'jsdoc-vuejs',
 	],
-	source: {
-		includePattern: '\\.(vue|js)$',
+	rules: {
+		// Conflict with Node 6 and Vue; remove when we upgrade to Node 8.	
+		'node/no-unsupported-features': 0,
+		'node/no-unsupported-features/es-syntax': 'off',
+		// https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-rules-check-tag-names
+		'jsdoc/check-tag-names': [
+			0, // severity 
+			{
+				definedTags: [
+					'vue-prop',
+					'vue-data',
+					'vue-computed',
+					'vue-event',
+				],
+			},
+		],
+		// `html-indent` does not currently support spaces & since we are all
+		// tabs everywhere, let's keep it that way!
+		'vue/html-indent': 0,
+		
 	},
+	// source: {
+	// 	includePattern: '\\.(vue|js)$',
+	// },
 };
